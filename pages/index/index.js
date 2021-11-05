5e38ec42180ef56e8a2cd9390a88a428970c663d
Ethan Chen <2512235663@qq.com>
2021-11-05 21:09:44 +0800

    map around Chengdu

---
+++ pages/index/index.js
@@ -0,0 +1,72 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    lat:30.64,
+    lon:104.08,
+  },
+  showGuide:function(){
+    var that=this;
+    wx.openLocation({
+      latitude: that.data.lat,
+      longitude: that.data.lon,
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
